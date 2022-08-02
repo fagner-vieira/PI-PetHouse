2cfdabf5573dcc83b57dd64c0b768ba48a45ae2f
lanasoliv <halana.oliveira@hotmail.com>
2022-08-02 19:44:21 -0300

    arrumando db

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "",
-    "database": "happyPet", 
+    "database": "mydb_pi", 
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
