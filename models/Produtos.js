b3fffa7819ba8e1631e34285ee1107c94b6aa7da
Karla <karla.frinka@gmail.com>
2022-08-02 20:13:49 -0300

    model produtos

--- models/Produtos.js
+++ models/Produtos.js
@@ -1,15 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-    const Produtos = sequelize.define("Produtos", {
-    racao: DataTypes.STRING,
-    sabor_racao: DataTypes.STRING,
-    peso_pacote_racao: DataTypes.STRING,
-    preco_venda: DataTypes.FLOAT,
-    created_at:DataTypes.DATE,
-    updated_at: DataTypes.DATE
- }, {
-     tableName: 'produtos',
-     timestamp: true
- });
+    "Produtos",
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      racao: {
+        type: DataTypes.STRING,
+      },
+      sabor_racao: {
+        type: DataTypes.STRING,
+      },
+      peso_pacote_racao: {
+        type: DataTypes.STRING,
+      },
+      peco_venda: {
+        type: DataTypes.STRING,
+      },
+     
+    },
+
+    {
+      tableName: "produtos",
+      timestamp: true,
+    };
 
  Produtos.associate = (listaDeModels) => {
     Produtos.belongToMany(listaDeModels.Pedido, {
